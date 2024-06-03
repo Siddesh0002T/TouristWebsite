085dbb0181f0525ce4911d01b7af9c51b96f8fa8
Suyash1354 <patilsuyash31@gmail.com>
2024-06-03 14:43:41 +0530

    Des

--- modules/home.php
+++ modules/home.php
@@ -9,6 +9,9 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Karantina:wght@300;400;700&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=REM:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Hind:wght@300;400;500;600;700&family=REM:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
     <title>TouristWebsite</title>
 </head>
 <body>
@@ -16,7 +19,9 @@
     <div class="Bar">
         <nav>
             <ul>
-            <li><a href="HistoryofNas.html">Tourist Website of Nashik</a></li>
+            <li><a href="HistoryofNas.php">History of Nashik</a></li>
+            <li><a href="destination.php">Destination</a></li>
+            <li><a href="HistoryofNas.html">Images</a></li>  
             
             </ul>
         </nav>
