516e398c55fc9b67454337f759297fa947c43774
Suyash1354 <patilsuyash31@gmail.com>
2024-06-03 14:16:01 +0530

    Update home.php

---
+++ modules/home.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Karantina:wght@300;400;700&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Karantina:wght@300;400;700&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=REM:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <title>TouristWebsite</title>
+</head>
+<body>
+
+    <div class="Bar">
+        <nav>
+            <ul>
+            <li><a href="HistoryofNas.html">Tourist Website of Nashik</a></li>
+            
+            </ul>
+        </nav>
+    </div>
+    
+    <h1>Nashik</h1>
+
+    
+    
+</body>
+</html>
