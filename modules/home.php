998c6f6c6ecddf62782cc19f9ee5a67431b62815
Siddhesh Ravindra More <94625517+Siddesh0002T@users.noreply.github.com>
2024-06-03 14:26:48 +0530

    Update home.php

--- modules/home.php
+++ modules/home.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost:8080/TouristWebsite/assets/css/styles.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Karantina:wght@300;400;700&display=swap" rel="stylesheet">
