1489f52496b83d520a038d8231322e0a661a5568
Siddhesh Ravindra More <94625517+Siddesh0002T@users.noreply.github.com>
2024-06-04 15:31:04 +0530

    Update gotologin.js

--- assets/js/gotologin.js
+++ assets/js/gotologin.js
@@ -5,3 +5,14 @@ function gotoLogin () {
     window.location = "../../modules/login.php";
 }
 */
+
+console.log("Hello World");
+// Get the button element
+const button = document.getElementById("myButton");
+
+// Attach the gotoLogin function to the button's click event
+button.addEventListener("click", gotoLogin);
+
+function gotoLogin () {
+window.location = 'login.php';
+}
