cbf9f7b32cf06ade50e2bc99e09f3fd04592632e
Siddhesh Ravindra More <94625517+Siddesh0002T@users.noreply.github.com>
2024-06-07 12:37:19 +0530

    Update login.php

--- modules/login.php
+++ modules/login.php
@@ -1,3 +1,29 @@
+<?php
+// connect to database
+include("../config/db_connect.php");
+
+if (isset($_POST['login'])) {
+    $uname = $_POST['uemail'];
+    $pass = $_POST['pass'];
+
+    // Query to retrieve user data
+    $query = "SELECT * FROM `tuser` WHERE `uemail` = '$uemail' AND `pass` = '$pass'";
+    $result = mysqli_query($conn, $query);
+
+    if (mysqli_num_rows($result) > 0) {
+        // User exists, login successful
+        $user_data = mysqli_fetch_assoc($result);
+        session_start();
+        $_SESSION['uname'] = $user_data['uname'];
+        $_SESSION['uemail'] = $user_data['uemail'];
+        header("Location: home.php",true);
+        exit();
+    } else {
+        // User does not exist or password is incorrect
+        $error = "Invalid username or password";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,7 +50,9 @@
     </div>
     <form>
         <h3>Login Here</h3>
-
+        <?php if (isset($error)) { ?>
+            <p style="color: red;"><?php echo $error; ?></p>
+        <?php } ?>
         <label for="username">Email</label>
         <input type="email" placeholder="Email id" id="username" name="uemail" required>
 
