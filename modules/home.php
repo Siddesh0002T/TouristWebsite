c03b39504e073d30e02d5df8fb3e3474cddba947
Siddhesh Ravindra More <94625517+Siddesh0002T@users.noreply.github.com>
2024-06-09 23:32:14 +0530

    Update home.php

--- modules/home.php
+++ modules/home.php
@@ -41,6 +41,7 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
     <?php 
         echo "<h1 style='color:#fff;'>".$_SESSION['uname'] ."</h1>";
         ?>
+    
     <button onclick=""  type="button" id="LogOutBtn">
         Logout
   </button>
@@ -101,7 +102,8 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
     
     
 
-   
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
   <script src="../assets/js/logout.js"></script>
 </body>
 </html>
