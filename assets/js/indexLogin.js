e388fd0c2960ebed81a261d0b750e9ef193a08d9
Siddhesh Ravindra More <94625517+Siddesh0002T@users.noreply.github.com>
2024-06-10 00:00:41 +0530

    index.html

--- assets/js/indexLogin.js
+++ assets/js/indexLogin.js
@@ -2,11 +2,11 @@
 console.log("i am login button which throw user to login page where user able to login by using thier email and pass if user is new on our website so user can able to register it self on our webbsite");
 console.log("Hello World");
 // Get the button element
-const button = document.getElementById("LogIn");
+const button = document.getElementById("LogInBtn");
 
 // Attach the gotoLogin function to the button's click event
 button.addEventListener("click", logIn);
 
-function logIn () {
-window.location = './modules/login.php';
+function logIn (){
+window.location = '/modules/login.php';
 }
