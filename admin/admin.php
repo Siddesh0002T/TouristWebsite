e38b1e19a21bcb37cab8485857aed49d21659203
Siddhesh Ravindra More <94625517+Siddesh0002T@users.noreply.github.com>
2024-06-11 00:48:45 +0530

    add login system for admin create table for admins

--- admin/admin.php
+++ admin/admin.php
@@ -1,3 +1,23 @@
 <?php
-   include './includes/header.php';
+session_start();
+
+if(!isset($_SESSION['adminloggedin']) || $_SESSION['adminloggedin']!=true){
+    header("location: adminlogin.php");
+    exit;
+}
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Admin</title>
+   <?php 
+        echo "Welcome ".$_SESSION['auname'] ."!";
+        ?>
+</head>
+<body>
+   
+</body>
+</html>
