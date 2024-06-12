44d62fe6de01ac5b744d9b0c05429a3112fd7bd2
Siddhesh Ravindra More <94625517+Siddesh0002T@users.noreply.github.com>
2024-06-12 13:01:41 +0530

    Update manageuser.php

--- admin/manageuser.php
+++ admin/manageuser.php
@@ -1,24 +1,16 @@
 <?php
+session_start();
+
+if(!isset($_SESSION['adminloggedin']) || $_SESSION['adminloggedin']!=true){
+    header("location: adminlogin.php");
+    exit;
+}
+
 $insert = false;
- // Database connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "NoteWeb_DB";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if (!$conn) {
-  die("Connection failed: ". $conn->connect_error);
-}
-else{
- // echo"Connected";
-}
+include './config/db_connect.php';
 if(isset($_GET['delete'])){
     $sno = $_GET['delete'];
-    $stmt = $conn->prepare("DELETE FROM `notes` WHERE `sno` =?");
+    $stmt = $conn->prepare("DELETE FROM `tuser` WHERE `id` =?");
     $stmt->bind_param("i", $sno);
     if ($stmt->execute()) {
         echo "Record deleted successfully";
@@ -26,16 +18,17 @@ if(isset($_GET['delete'])){
         echo "Error: ". $stmt->error;
     }
 }
+// Alter User By Admin
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (isset($_POST['snoEdit'])) {
+   if (isset($_POST['snoEdit'])) {
         // update the record
-        $sno = $_POST["snoEdit"];
-        $title = $_POST['titleEdit'];
-        $description = $_POST['descriptionEdit'];
+        $id = $_POST["snoEdit"];
+        $uname = $_POST['unameEdit'];
+        $uemail = $_POST['uemailEdit'];
         $insert = false;
-        if ($title!= "") {
-            $stmt = $conn->prepare("UPDATE `notes` SET `title` =?, `description` =? WHERE `notes`.`sno` =?");
-            $stmt->bind_param("ssi", $title, $description, $sno);
+        if ($uname!= "") {
+            $stmt = $conn->prepare("UPDATE `tuser` SET `uname` =?, `uemail` =? WHERE `tuser`.`id` =?");
+            $stmt->bind_param("ssi", $uname, $uemail, $idS);
             if ($stmt->execute()) {
                 //echo '<p>New record created successfully  </p>';
                 $insert = true;
@@ -43,23 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 echo "Error: ". $stmt->error;
             }
         }
-    } else {
-        $title = $_POST['title'];
-        $description = $_POST['description'];
-        $insert = false;
-        if ($title!= "") {
-            $stmt = $conn->prepare("INSERT INTO `notes`(`title`, `description`, `tstamp`) VALUES (?,?, NOW())");
-            $stmt->bind_param("ss", $title, $description);
-            if ($stmt->execute()) {
-                //echo '<p>New record created successfully  </p>';
-                $insert = true;
-            } else {
-                echo "Error: ". $stmt->error;
-            }
-        } else {
-            echo "empty";
-        }
-    }
+    } 
 }
 
 ?>
@@ -70,11 +47,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>NoteWeb</title>
+    <title>Manageusers</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="datatables.min.css" >
+    <link rel="stylesheet" href="./assets/css/dataTables/datatables.min.css" >
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.min.css">
    
 </head>
@@ -83,34 +60,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
+    <form action="manageuser.php" method="post">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editModalLabel">Edit Note</h1>
+        <h1 class="modal-title fs-5" id="editModalLabel">Alter User</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-      <form action="index.php" method="post">
+     
         <input type="hidden" name="snoEdit" id="snoEdit"> 
                     <div class="mb-3">
-                        <label for="title" class="form-label">Title</label>
-                        <input type="text" class="form-control" id="titleEdit" name="titleEdit" required>
+                        <label for="unameEdit" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="unameEdit" name="unameEdit" required>
                     </div>
                     <div class="mb-3">
-                        <label for="description" class="form-label">Description</label>
-                        <textarea class="form-control" id="descriptionEdit" name="descriptionEdit" rows="3" required></textarea>
+                        <label for="uemailEdit" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="uemailEdit" name="uemailEdit"  required>
                     </div>
-                    <button type="submit" class="btn btn-primary">Edit Note</button>
-                </form>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="submit" class="btn btn-primary">Alter User</button>
       </div>
+      </form>
     </div>
   </div>
 </div>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">NoteWeb</a>
+            <a class="navbar-brand" href="#">TouristWebsite</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
@@ -119,52 +96,58 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        <a class="nav-link active" aria-current="page" href="admin.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php">About Us</a>
+                  
                     </li>
                 </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <div class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search"class="dt-input">
                     <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                </div>
             </div>
         </div>
     </nav>
-
+    <?php 
+        echo "Welcome ".$_SESSION['auname'] ."!";
+        ?>
     <div class="container">
         <div class="row">
-           
+        <hr>
+            <hr>
+            <hr>
+            <br>
+           <h1>Manage Users</h1>
             <br>
             <hr>
             <hr>
             <hr>
             <div class="col-md-12">
 
-                <table class="table" id="myTable">
+                <table class="table" id="myUserManage">
                     <thead>
                         <tr>
-                            <th scope="col">No.</th>
-                            <th scope="col">Title</th>
-                            <th scope="col">Description</th>
+                            <th scope="col">Sr.No.</th>
+                            <th scope="col">Name </th>
+                            <th scope="col">Email</th>
                             <th scope="col">Date And Time</th>
                             <th scope="col">Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php
-                        $sql = "SELECT * FROM notes";
+                        $sql = "SELECT * FROM tuser";
                         $result = mysqli_query($conn, $sql);
                         $sno = 0;
                         while ($row = mysqli_fetch_assoc($result)) {
                             $sno = $sno + 1;
                             echo "<tr>
-                                    <th scope='row'>".$sno." </th>
-                                    <td>" . $row['title'] . "</td>
-                                    <td>" . $row['description'] . "</td>
-                                    <td>" . $row['tstamp'] . "</td>
-                                    <td><button class='btn btn-sm btn-primary edit' id=".$row['sno'].">Edit</button> <button class='btn btn-sm btn-primary delete' id=d".$row['sno'].">Delete</button></td>
+                                    <td scope='row'>".$sno." </td>
+                                    <td>" . $row['uname'] . "</td>
+                                    <td>" . $row['uemail'] . "</td>
+                                    <td>" . $row['register_date'] . "</td>
+                                    <td><button class='btn btn-sm btn-primary edit' id=".$row['id'].">Edit</button> <button class='btn btn-sm btn-danger delete' id=d".$row['id'].">Delete</button></td>
                                 </tr>";
                         }
                         ?>
@@ -183,12 +166,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 
-    <script src="datatables.min.js"></script>
+    <script src="./assets/js/dataTables/datatables.min.js"></script>
     <script src="https://cdn.datatables.net/2.0.8/js/dataTables.dataTables.min.js"></script>
 
     <script>
         $(document).ready(function() {
-            $('#myTable').DataTable();
+            $('#myUserManage').DataTable();
         });
 
         const myModal = document.getElementById('editModal')
@@ -204,12 +187,12 @@ myModal.addEventListener('shown.bs.modal', () => {
             element.addEventListener("click", (e)=>{
                 console.log("edit", );
                 tr = e.target.parentNode.parentNode;
-                title = tr.getElementsByTagName("td")[0].innerText;
-                description = tr.getElementsByTagName("td")[1].innerText;
-                console.log(title,description);
-                titleEdit.value = title;
-                descriptionEdit.value = description;
-                snoEdit.value = e.target.id;
+                uname = tr.getElementsByTagName("td")[1].innerText;
+                uemail = tr.getElementsByTagName("td")[2].innerText;
+                console.log(uname,uemail);
+                unameEdit.value = uname;
+                uemailEdit.value = uemail;
+                snoEdit.value = e.target.sno;
                 console.log("Btn Sno : ",e.target.id);
                 $('#editModal').modal('toggle');
         })
@@ -217,11 +200,11 @@ myModal.addEventListener('shown.bs.modal', () => {
         deletes = document.getElementsByClassName('delete');
 Array.from(deletes).forEach((element)=>{
     element.addEventListener("click", (e)=>{
-        console.log("edit", );
+        console.log("delete", );
         sno = e.target.id.substr(1,);
         if(confirm("Delete Note !")){
             console.log("yes");
-            window.location = 'index.php?delete=' + sno;
+            window.location = 'manageuser.php?delete=' + sno;
         }
         else{
             console.log("no");
